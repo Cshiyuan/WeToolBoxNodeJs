8e55ff45af38c5654a019acbab33afc7f3eb17ba
shiyuan chen <chenshyiuan@qq.com>
2018-01-24 15:12:52 +0800

    【更改说明】删除多余文件

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const cos = require('../utils/cos')
-
-let multer = require('multer')
-let upload = multer({dest: '../tmp/'})
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -13,18 +9,6 @@ router.get('/', function (req, res, next) {
 
 });
 
-
-/* GET home page. */
-router.use('/upload', upload.single('file'), function (req, res, next) {
-
-
-    console.log(req)
-    cos.uploadFile(req.file).then(data => {
-        console.log(data)
-        console.log('https://'+ data.Location)
-    })
-    res.json('asd')
-});
 
 
 module.exports = router;
